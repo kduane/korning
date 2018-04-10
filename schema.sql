49a67af51d33a567826bb989e9333e158b15029d
kduane <karl.duane@gmail.com>
2018-04-10 16:38:31 -0400

    defined SQL schema

--- schema.sql
+++ schema.sql
@@ -1,1 +1,29 @@
 -- DEFINE YOUR DATABASE SCHEMA HERE
+
+CREATE TABLE customer (
+  id SERIAL PRIMARY KEY,
+  customer VARCHAR(255),
+  account_no VARCHAR(255)
+);
+
+CREATE TABLE employee (
+  id SERIAL PRIMARY KEY,
+  employee VARCHAR(255)
+);
+
+CREATE TABLE product (
+  id SERIAL PRIMARY KEY,
+  product_name VARHAR(255)
+);
+
+CREATE TABLE invoice (
+  id SERIAL PRIMARY KEY,
+  invoice_no INTEGER,
+  sale_date DATE,
+  units_sold INTEGER,
+  sale_amount MONEY,
+  invoice_frequency VARCHAR(50),
+  product_id INTEGER,
+  customer_id INTEGER,
+  employee_id INTEGER,
+);
