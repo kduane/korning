964b103c58386f48fd7549b80df09c7fcc656c11
kduane <karl.duane@gmail.com>
2018-04-10 17:55:24 -0400

    planned out steps in comments

--- import.rb
+++ import.rb
@@ -1,7 +1,8 @@
 # Use this file to import the sales information into the
 # the database.
-
+require 'csv'
 require "pg"
+require 'pry'
 
 def db_connection
   begin
@@ -9,5 +10,41 @@ def db_connection
     yield(connection)
   ensure
     connection.close
+  end
+end
+
+db_connection do |conn|
+  CSV.foreach("sales.csv", :headers => true) do |row|
+    # get row data and define with variables
+    # split employee into name and email
+    # split customer into name and account_no
+    # INSERT into:
+        # customer table
+          # name
+          # account_no
+          # NOTE: ensure no duplication
+        # employee table
+          # employee_name
+          # email
+          # NOTE: ensure no duplication
+        # product table
+          # product_name
+          # NOTE: ensure no duplication
+
+    # gather foreign keys from:
+        # customer
+        # employee
+        # product
+
+    # populate invoice table
+        # invoice_no
+        # sale_date
+        # units_sold
+        # sale_amount
+        # invoice_frequency
+        # product_id
+        # customer_id
+        # employee_id
+
   end
 end
--- schema.sql
+++ schema.sql
@@ -2,13 +2,14 @@
 
 CREATE TABLE customer (
   id SERIAL PRIMARY KEY,
-  customer VARCHAR(255),
+  customer_name VARCHAR(255),
   account_no VARCHAR(255)
 );
 
 CREATE TABLE employee (
   id SERIAL PRIMARY KEY,
-  employee VARCHAR(255)
+  employee_name VARCHAR(255),
+  email VARCHAR(255)
 );
 
 CREATE TABLE product (
